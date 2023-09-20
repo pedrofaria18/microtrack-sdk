73eff2ce9568838b34ddd1f2439dee21c275dd6a
pedrofaria18 <pedrof.andrade16@gmail.com>
2023-09-19 21:29:49 -0300

    Add first request for central api

--- src/main/java/org/microtrack/Manager.java
+++ src/main/java/org/microtrack/Manager.java
@@ -1,5 +1,12 @@
 package org.microtrack;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Manager {
     private final String serviceName;
     private String traceId;
@@ -11,7 +18,22 @@ public class Manager {
         this.genericData = genericData;
     }
 
-    public String getDataMessage() {
+    public void sendTrace(String serviceName, String traceId, Object genericData) throws Exception {
+        String uri = "http://localhost:3000/trace";
+
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(String.format("{\"serviceName\": \"%s\", \"traceId\": \"%s\", \"genericData\": \"%s\"}", serviceName, traceId, genericData)))
+                .build();
+
+        HttpResponse<?> response = client.send(request, HttpResponse.BodyHandlers.discarding());
+        System.out.println(response.statusCode());
+    }
+
+    public String getDataMessage() throws Exception {
+        sendTrace(serviceName, traceId, genericData);
         return String.format("%s, %s, %s!", serviceName, traceId, genericData);
     }
 }
